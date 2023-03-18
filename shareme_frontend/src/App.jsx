fb43c8ba935ee6c5301b125d0aa3f30acfd4ac0b
unknown <fkt2119@gmail.com>
2023-03-18 10:38:15 +0300

    first commit

---
+++ shareme_frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+const App = () => {
+
+    return (
+        <>
+            <div>
+                <h1 className="text-3xl font-bold underline">
+                    hello there
+                </h1>
+
+            </div>
+
+
+        </>
+    )
+
+}
+
+export default App
